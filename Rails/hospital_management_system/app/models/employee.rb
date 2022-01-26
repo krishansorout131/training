c8f1a4dba346c9d248ecc787fa394cc951e9cee1
krishansorout131 <krishansorout131@gmail.com>
2022-01-26 12:46:24 +0530

    Add migration comparison on employees table

--- Rails/hospital_management_system/app/models/employee.rb
+++ Rails/hospital_management_system/app/models/employee.rb
@@ -1,4 +1,5 @@
 class Employee < ApplicationRecord
-    validates :salary, presence: true 
-    validates :specialization, presence: true
+  validates :salary, presence: true 
+  validates :specialization, presence: true
+  validates :salary, comparison: { greater_than_or_equal_to: 10000 }
 end
