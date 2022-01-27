8ec10b84ea760c263aa001c4f1b777e3c741b557
krishansorout131 <krishansorout131@gmail.com>
2022-01-27 13:33:24 +0530

    Add strict validation on specialization column of employees table

--- Rails/hospital_management_system/app/models/employee.rb
+++ Rails/hospital_management_system/app/models/employee.rb
@@ -1,9 +1,9 @@
 class Employee < ApplicationRecord
   validates :salary, presence: true 
-  #validates :specialization, presence: true
+  validates :specialization, presence: { strict: true }
   validates :salary, comparison: { greater_than_or_equal_to: 10000 }
   validates :name, format: { with: /\A[a-zA-Z]+\z/}
   validates :salary, numericality: { only_integer: true}
-  validates :specialization, absence: true
+  #validates :specialization, absence: true
   validates :name, uniqueness: true
 end
