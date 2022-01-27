4443785a0a53aa9f856751e80566fc6ded23f695
krishansorout131 <krishansorout131@gmail.com>
2022-01-27 12:23:17 +0530

    Add absence validation

--- Rails/hospital_management_system/app/models/employee.rb
+++ Rails/hospital_management_system/app/models/employee.rb
@@ -1,8 +1,8 @@
 class Employee < ApplicationRecord
   validates :salary, presence: true 
-  validates :specialization, presence: true
+  #validates :specialization, presence: true
   validates :salary, comparison: { greater_than_or_equal_to: 10000 }
   validates :name, format: { with: /\A[a-zA-Z]+\z/}
   validates :salary, numericality: { only_integer: true}
-  
+  validates :specialization, absence: true
 end
