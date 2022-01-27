71f6683955235a0f01d474c43ae00e68bccb17e8
krishansorout131 <krishansorout131@gmail.com>
2022-01-27 13:20:22 +0530

    Add validatiwith option allow_blank on addres column in patients table

--- Rails/hospital_management_system/app/models/patient.rb
+++ Rails/hospital_management_system/app/models/patient.rb
@@ -1,5 +1,5 @@
 class Patient < ApplicationRecord
   validates :address, exclusion: { in: %w(delhi gurugram), message: "%{value} is reserved." }
-  validates :address, inclusion: { in: %w(hodal palwal faridabad), message: "not accepting %{value}"}, allow_nil: true
+  validates :address, inclusion: { in: %w(hodal palwal faridabad), message: "not accepting %{value}"}, allow_blank: true #allow_nil: true
   validates :name, length: { minimum: 3 , message: "name should be greater then 3 words"}
 end
