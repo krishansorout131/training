02c70dca93057f391168d05f7b3f475b2a860681
krishansorout131 <krishansorout131@gmail.com>
2022-01-27 11:21:05 +0530

    Add validation on address column of patients table

--- Rails/hospital_management_system/app/models/patient.rb
+++ Rails/hospital_management_system/app/models/patient.rb
@@ -1,3 +1,4 @@
 class Patient < ApplicationRecord
   validates :address, exclusion: { in: %w(delhi gurugram), message: "%{value} is reserved." }
+  validates :address, inclusion: { in: %w(hodal palwal faridabad), message: "not accepting %{value}"}
 end
