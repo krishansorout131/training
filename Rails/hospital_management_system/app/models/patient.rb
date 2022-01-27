ec39aef90ab3988f7519d4fb097f570a52b1766f
krishansorout131 <krishansorout131@gmail.com>
2022-01-27 11:44:25 +0530

    Add validation length on name column of patients table

--- Rails/hospital_management_system/app/models/patient.rb
+++ Rails/hospital_management_system/app/models/patient.rb
@@ -1,4 +1,5 @@
 class Patient < ApplicationRecord
   validates :address, exclusion: { in: %w(delhi gurugram), message: "%{value} is reserved." }
   validates :address, inclusion: { in: %w(hodal palwal faridabad), message: "not accepting %{value}"}
+  validates :name, length: { minimum: 3 , message: "name should be greater then 3 words"}
 end
