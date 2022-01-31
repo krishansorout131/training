65106bcc17a46511b4debdd82013bad0b5e46ece
krishansorout131 <krishansorout131@gmail.com>
2022-01-31 10:56:29 +0530

    Add self join in employees table

--- Rails/hospital_management_system/app/models/employee.rb
+++ Rails/hospital_management_system/app/models/employee.rb
@@ -2,10 +2,10 @@ class Employee < ApplicationRecord
   validates :salary, presence: true 
   validates :specialization, presence: { strict: true }
   validates :salary, comparison: { greater_than_or_equal_to: 10000 }
-  validates :name, format: { with: /\A[a-zA-Z]+\z/}
+  #validates :name, format: { with: /\A[a-zA-Z]+\z/}
   validates :salary, numericality: { only_integer: true}
   #validates :specialization, absence: true
-  validates :name, uniqueness: true
+  #validates :name, uniqueness: true
   belongs_to :department
   #has_many :opd_details
   #has_many :patients, through: :opd_details
@@ -13,4 +13,6 @@ class Employee < ApplicationRecord
   has_one :patient, through: :opd_detail
   has_many :feedbacks, as: :feedback
   has_and_belongs_to_many :patients
+  has_many :employees, class_name: "Employee",foreign_key: "manager_id"
+  belongs_to :manager, class_name: "Employee", optional: true
 end
