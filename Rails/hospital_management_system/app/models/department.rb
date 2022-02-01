0a3c5457cc4f114a42828cb98efb11aa6c8c09d5
krishansorout131 <krishansorout131@gmail.com>
2022-02-01 16:26:17 +0530

    Add around_create and after_create callbacks on department table

--- Rails/hospital_management_system/app/models/department.rb
+++ Rails/hospital_management_system/app/models/department.rb
@@ -2,9 +2,19 @@ class Department < ApplicationRecord
   has_many :employees  
   has_many :feedbacks, as: :feedback
   before_create :downcase_check
+  after_create :after_capitalize
+  around_create :around_upcase
 
   private
   def downcase_check
     self.name.downcase! 
+  end  
+  def around_upcase
+    self.name.capitalize!
+    yield
+    puts "new value saved #{self.name}"
+  end  
+  def after_capitalize
+    self.name.upcase!
   end  
 end
