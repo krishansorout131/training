fa8db09065794716daef9d55a379654c1d4f91cd
krishansorout131 <krishansorout131@gmail.com>
2022-02-01 18:44:36 +0530

    Add before_destroy callback in opd_details table

--- Rails/hospital_management_system/app/models/opd_detail.rb
+++ Rails/hospital_management_system/app/models/opd_detail.rb
@@ -1,4 +1,11 @@
 class OpdDetail < ApplicationRecord
   belongs_to :employee
-  belongs_to :patient  
+  belongs_to :patient 
+  before_destroy :destroy_details
+  
+  private
+
+  def destroy_details
+    puts "Thankyou #{self.patient.name} for taking treatment from our hospital"
+  end  
 end
