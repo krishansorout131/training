e6c91df75f265b3581b8d1863a3338da38cbf8bd
krishansorout131 <krishansorout131@gmail.com>
2022-02-01 12:42:21 +0530

    Add before_create callback on patients table

--- Rails/hospital_management_system/app/models/department.rb
+++ Rails/hospital_management_system/app/models/department.rb
@@ -1,4 +1,10 @@
 class Department < ApplicationRecord
   has_many :employees  
   has_many :feedbacks, as: :feedback
+  before_create :downcase_check
+
+  private
+  def downcase_check
+    self.name.downcase! 
+  end  
 end
