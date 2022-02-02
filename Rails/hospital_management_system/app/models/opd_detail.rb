e7f2c0f5f11f2c815d3a3dc8c7e27b9539cb721b
krishansorout131 <krishansorout131@gmail.com>
2022-02-02 12:19:04 +0530

    Add after_touch callback in OpdDetail model

--- Rails/hospital_management_system/app/models/opd_detail.rb
+++ Rails/hospital_management_system/app/models/opd_detail.rb
@@ -2,9 +2,12 @@ class OpdDetail < ApplicationRecord
   belongs_to :employee
   belongs_to :patient 
   before_destroy :destroy_details
-  
+  after_touch :touched
   private
-
+  
+  def touched
+    puts "you have touched an object"
+  end  
   def destroy_details
     puts "Thankyou #{self.patient.name} for taking treatment from our hospital"
   end  
