f856f0059e59d998a988676117cf4d7ad01d2339
krishansorout131 <krishansorout131@gmail.com>
2022-02-03 17:14:21 +0530

    Add updated departments_controller.rb file

--- Rails/hospital_management_system/app/controllers/departments_controller.rb
+++ Rails/hospital_management_system/app/controllers/departments_controller.rb
@@ -3,5 +3,8 @@ class DepartmentsController < ApplicationController
   def index
     @departments = Department.all
   end  
-
+  def department_employees
+    @employees = Department.find(params[:id]).employees
+    @department_name = Department.find(params[:id]).name
+  end  
 end
