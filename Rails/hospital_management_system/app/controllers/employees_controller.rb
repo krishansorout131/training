4ee83b4afd7413f020c66f9a01a9d546707f9d7f
krishansorout131 <krishansorout131@gmail.com>
2022-02-04 15:50:35 +0530

    update employees_controller file

--- Rails/hospital_management_system/app/controllers/employees_controller.rb
+++ Rails/hospital_management_system/app/controllers/employees_controller.rb
@@ -10,5 +10,19 @@ class EmployeesController < ApplicationController
   def manager_subordinates
     @manager_employees = Employee.find(params[:id]).employees
     @manager_name = Employee.find(params[:id]).name
+    @manager_details = Employee.find(params[:id])
+  end
+  def employee_details
+    @employee = Employee.find(params[:id])
   end  
+  def doctors
+    @doctors = Employee.where(post: "Doctor")
+  end  
+  def doctor_patients
+    @doctor = Employee.find(params[:id])
+    @doctor_patients = Employee.find(params[:id]).patients
+  end  
+  def doctor_details
+    @employee = Employee.find(params[:id])
+  end 
 end
