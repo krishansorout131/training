6ad1a4a4c0e4cb71fcf20955abf88e26b38e82bb
krishansorout131 <krishansorout131@gmail.com>
2022-02-04 15:56:08 +0530

    Update routes file

--- Rails/hospital_management_system/config/routes.rb
+++ Rails/hospital_management_system/config/routes.rb
@@ -7,4 +7,11 @@ Rails.application.routes.draw do
   get "/department/:id" => "departments#department_employees"
   get "/employees/managers" => "employees#managers"
   get "/employees/managers/:id" => "employees#manager_subordinates"
+  get "/employees/:id" => "employees#employee_details"
+  get "/patients" => "patients#patients"
+  get "/doctors" => "employees#doctors"
+  get "/doctors/:id" => "employees#doctor_patients"
+  get "/patients/:id" => "patients#patient_details"
+  get "/patient/doctors/:id" => "employees#doctor_details"
+  get "/doctors/patients/:id" => "patients#doctor_patient_details" 
 end
