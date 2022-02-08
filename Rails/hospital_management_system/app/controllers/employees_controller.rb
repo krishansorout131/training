fb14c4fa5a50d210dcf4f1edfd178dc3be56385f
krishansorout131 <krishansorout131@gmail.com>
2022-02-08 18:05:39 +0530

    update employees_controller

--- Rails/hospital_management_system/app/controllers/employees_controller.rb
+++ Rails/hospital_management_system/app/controllers/employees_controller.rb
@@ -12,6 +12,7 @@ class EmployeesController < ApplicationController
     @manager_name = Employee.find(params[:id]).name
     @manager_details = Employee.find(params[:id])
   end
+
   def employee_details
     @employee = Employee.find(params[:id])
   end  
@@ -27,9 +28,47 @@ class EmployeesController < ApplicationController
     @idp = params[:idp]
   end 
 
+  def employee_form
+    @employee = Employee.new
+    debugger
+  end
+
   def create
-    @employee = Employee.new(name: params[:name],dob: params[:dob], gender: params[:gender],specialization: params[:specialization],salary: params[:salary],department_id: params[:department_id], post: params[:post],manager_id: params[:manager_id])
-    @employee.save
-    redirect_to "/employees/#{@employee.id}"
+    @employee = Employee.new(employee_params)
+    debugger
+    # @employee = Employee.new(name: params[:name],dob: params[:dob], gender: params[:gender],specialization: params[:specialization],salary: params[:salary],department_id: params[:department_id], post: params[:post],manager_id: params[:manager_id])
+    if @employee.save
+      redirect_to "/employees/#{@employee.id}"
+    else
+      render :employee_form, status: :unprocessable_entity 
+    end
   end  
+  
+
+  def destroy
+    employee = Employee.find(params[:id])
+    employee.destroy
+    redirect_to :employees
+  end
+ 
+
+  def edit
+    @employee = Employee.find(params[:id])
+  end
+
+  def update
+    @employee = Employee.update(employee_params)
+
+    # @employee = Employee.new(name: params[:name],dob: params[:dob], gender: params[:gender],specialization: params[:specialization],salary: params[:salary],department_id: params[:department_id], post: params[:post],manager_id: params[:manager_id])
+   # if @employee.save
+      redirect_to "/employees/#{@employee.id}"
+    #else
+      render :employee_form, status: :unprocessable_entity 
+   # end
+  end
+  private
+
+  def employee_params
+    params.require(:employee).permit(:name, :gender, :dob, :specialization, :salary, :department_id, :post, :manager_id)
+  end
 end
