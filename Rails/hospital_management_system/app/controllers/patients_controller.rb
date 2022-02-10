c2adce52c3fa014edba4ca14467548a9614e10dc
krishansorout131 <krishansorout131@gmail.com>
2022-02-10 11:46:48 +0530

    Add destroy action in patients_controller

--- Rails/hospital_management_system/app/controllers/patients_controller.rb
+++ Rails/hospital_management_system/app/controllers/patients_controller.rb
@@ -14,14 +14,37 @@ class PatientsController < ApplicationController
   end  
 
   def create
-     @patient = Patient.create(patient_params)
+    @patient = Patient.create(patient_params)
 
-     if @patient.save
+    if @patient.save
       redirect_to "/patients/#{@patient.id}"
     else
       render :new, status: :unprocessable_entity 
     end
   end
+
+  def edit
+    @patient = Patient.find(params[:id])
+  end
+  
+  def update 
+    @patient = Patient.find(params[:id])
+    respond_to do |format|
+      if @patient.update(patient_params)
+        format.html { redirect_to patient_url(@patient), notice: "patient was successfully updated." }
+        format.json { render :show, status: :ok, location: @patient }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @patient.errors, status: :unprocessable_entity }
+      end
+    end
+  end  
+
+  def destroy
+    @patient = Patient.find(params[:id])
+    @patient.destroy
+    redirect_to patients_url
+  end  
 
   def doctor_patient_details
     @patient = Patient.find(params[:id])
