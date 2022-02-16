fd30bffdbe87ba53ce54e68c4d5d3c072d751461
krishansorout131 <krishansorout131@gmail.com>
2022-02-16 18:30:27 +0530

    Add user.rb model

---
+++ Rails/Assignments/device_gem_product_management/app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :trackable
+
+         private
+         def after_confirmation
+          WelcomeMailer.send_greetings(self).deliver_now
+         end
+end
