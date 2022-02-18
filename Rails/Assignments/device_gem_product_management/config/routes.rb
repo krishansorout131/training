bfee844c82056d2c30df858129c73fb87163e62e
krishansorout131 <krishansorout131@gmail.com>
2022-02-18 13:01:18 +0530

    updated routes for users

--- Rails/Assignments/device_gem_product_management/config/routes.rb
+++ Rails/Assignments/device_gem_product_management/config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  resources :products
+  root 'home#index'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     passwords: 'users/passwords',
     registrations: 'users/registrations'
   }
-  root 'home#index'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
